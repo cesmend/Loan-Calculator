04acbd5ff413a0c97683a5cc36f163fad7e9f877
Cesar Mendoza <cesarmendoza@Cesars-MBP.fritz.box>
2022-10-02 15:46:20 +0200

    Corrected comments in the index.js file

--- index.js
+++ index.js
@@ -4,39 +4,36 @@
 //npm install express
 //npm install nedb
 
-//use the Express package via node and import it here:
-const express = require("express");
-//import nedb (the database package)
-const Datastore = require("nedb");
-//create an variable that uses express
-const app = express();
+const express = require("express"); //use the Express package via node and import it here:
+const Datastore = require("nedb"); //import nedb (the database package)
+const app = express(); //create an variable that uses express
+
 //listen through the port 3000 for me to access it through the browser (localhost:3000)
-// add a port so that it doesn't use one arbitrarily, callback is a message about listening
+// add a port so that it doesn't use one arbitrarily, callback is a message about listening (console log)
 app.listen(3000, () => console.log("listening at 3000"));
+
 //use express to host static files for the server
-//the static files that will be served are in the public folder of this server
-app.use(express.static("public"));
-//limit the size of the json input in the server
-app.use(express.json({ limit: "1mb" }));
+app.use(express.static("public")); //the static files that will be served are in the public folder of this server
+app.use(express.json({ limit: "1mb" })); //limit the size of the json input in the server
 
 //give the database a path to where it is
-//in this case root folder in the file transactions.db
-const database = new Datastore("transactions.db");
-//load or create (if it was not there before)
-database.loadDatabase();
+const database = new Datastore("transactions.db"); //in this case root folder in the file transactions.db
+database.loadDatabase(); //load or create (if it was not there before)
 
-// ------------------ REQUESTS ------------------
+// ------------------ REQUEST HANDLING ------------------
 
 // POST
-//setup a route
-//I want to receive the post in api
-//then setup a callback by creating a function that has request and response as params
+//setup a route (I want to receive the post in /api)
+//setup a callback by creating a function that has request and response as parameteres
 app.post("/api", (request, response) => {
   const data = request.body;
-  const timestamp = Date.now();
-  data.timestamp = timestamp;
-  database.insert(data);
-  response.json(data);
+
+  //include the timestamp when posting to the database
+  const timestamp = Date.now(); //date right now
+  data.timestamp = timestamp; //timestamp value will be the date now
+
+  database.insert(data); //insert the data into the db
+  response.json(data); //return the data in json format
 });
 
 //GET
@@ -44,12 +41,10 @@ app.get("/api", (request, response) => {
   //test response
   // response.json({ test: 123 });
 
-  // Query all the database (find({})) and sort by timestamp
-  // can be limited if .limit(n) is added after sort
   database
-    .find({})
-    .sort({ timestamp: 1 })
+    .find({}) //query all the db
+    .sort({ timestamp: 1 }) //order by timestamp
     .exec(function (err, data) {
-      response.json(data);
+      response.json(data); //return the data in json format
     });
 });
